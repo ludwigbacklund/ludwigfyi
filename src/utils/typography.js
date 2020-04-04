0a846dde340abda8e1a20c2b9ef980256c1d3cce
Ludwig Bäcklund <ludwigbacklund@gmail.com>
2020-04-04 13:25:25 +0200

    Initial commit with typography.js and css normalize

---
+++ src/utils/typography.js
@@ -0,0 +1,11 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  googleFonts: [{ name: "Cabin", styles: [] }],
+  headerFontFamily: ["Cabin", "Arial", "sans-serif"],
+  bodyFontFamily: ["Georgia", "serif"],
+})
+
+export default typography
